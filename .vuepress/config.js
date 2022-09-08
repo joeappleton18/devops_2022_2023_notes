cc4d6a2aee9b224be16b7d9d3429cf0f3016cbc9
Joe Appleton <joe.appleton@solent.ac.uk>
2022-09-08 12:45:25 +0100

    bot commit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,32 +1,6 @@
-// module.exports = {
-//   themeConfig: {
-//     sidebar: [
-//       {
-//         tite: "Assessment Information",
-//         collapsable: true, 
-//         children: ["/assessment/scenario.md"],
-//       },
-//       {
-//         title: "Module Overview",
-//         collapsable: true,
-//         children: ["/"],
-//       },
-//       {
-//         title: "What is DevOps ",
-//         collapsable: true,
-//         children: ["/week-1.md"],
-//       },
-
-//       {
-//         title: "Conventions Used",
-//         collapsable: true,
-//         children: ["/conventions.md"],
-//       },
-//     ],
-//   },
-// };
-
 module.exports = {
+  base: "/devops_2022_2023_notes/",
+  dest: "docs",
   themeConfig: {
     sidebar: [
       {
@@ -39,7 +13,8 @@ module.exports = {
         collapsable: true,
         sidebarDepth: 1,    // optional, defaults to 1
         children: [
-          '/assessment/project_report_brief.md'
+          '/assessment/scenario',
+          
         ]
       },
     ]
